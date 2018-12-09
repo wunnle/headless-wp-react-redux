8852f43449bf54122d279b06441d2999038c837e
Sinan <me@wunnle.com>
2018-12-09 22:59:56 +0300

    new logo and header adjustments

--- src/components/Blog/common/Header.js
+++ src/components/Blog/common/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../../../img/papership.svg';
+import logo from '../../../img/logo.svg';
 
 export const Header = (props) => (
 <header>
